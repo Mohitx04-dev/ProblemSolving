86f3f329b211b2f94363db67f32149d3ae929c57
Mohit Chourasia <78469901+Mohitx04-dev@users.noreply.github.com>
2022-05-30 16:55:13 +0530

    Time: 538 ms (5.00%), Space: 55.4 MB (63.21%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,19 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
         int c = 0;
         int cur = nums[0];
         for(int x : nums) {
+             if(c==0) {
+                cur = x;
+            }
+            System.out.println(x + " " + c);
             if(cur==x) {
                 c++;
             }
             else {
-                cur = x;
-            }
-            if(c>nums.length/2){
-                return x;
+                c--;
             }
         }
-        return 0;
+        return cur;
     }
 }
