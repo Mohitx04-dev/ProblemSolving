791115592ce269124197cc1c20da00609e5d213e
Mohit Chourasia <78469901+Mohitx04-dev@users.noreply.github.com>
2022-06-04 00:55:17 +0530

    Time: 7 ms (11.24%), Space: 47.5 MB (8.15%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashMap<ListNode,Integer> map = new HashMap();
+        while(head!=null){
+            if(map.get(head)!=null){
+                return true;
+            }
+            else{
+                map.put(head,1);
+                head = head.next;
+            }
+        }
+        return false;
+    }
+}
