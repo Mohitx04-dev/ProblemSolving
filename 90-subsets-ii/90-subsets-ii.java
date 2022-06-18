d1d4499035115daf21c2be3309ffa0c709f2a0bc
Mohit Chourasia <78469901+Mohitx04-dev@users.noreply.github.com>
2022-06-18 20:09:43 +0530

    Time: 4 ms (24.15%), Space: 45.2 MB (6.18%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    List<List<Integer>> ans;
+    HashMap<List<Integer>, Integer> map;
+    void sub(List<Integer> op, List<Integer> ip){
+        if(ip.size()==0) {
+            Collections.sort(op);
+            if(map.get(op)==null) {
+                ans.add(op); 
+                map.put(op,1);
+            } 
+            return;
+        }
+        List<Integer> op1 = new ArrayList(op);
+        List<Integer> op2 = new ArrayList(op);
+        List<Integer> ip1 = new ArrayList(ip);
+        op2.add(ip1.get(0));
+        ip1.remove(0);
+        sub(op1,ip1);
+        sub(op2,ip1);
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        ans = new ArrayList();
+        map = new HashMap();
+        List<Integer> arr = new ArrayList();
+        for(int x:nums) arr.add(x);
+        List<Integer> op = new ArrayList();
+        sub(op,arr);
+        return ans;
+    }
+}
