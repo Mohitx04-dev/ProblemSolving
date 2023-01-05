4453b893562db19110c4f971a4ffe1fef1250c05
Mohit Chourasia <78469901+Mohitx04-dev@users.noreply.github.com>
2023-01-05 21:54:40 +0530

    Added solution - LeetHub

---
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            int k = Integer.parseInt(br.readLine().trim());
+            
+            Compute obj = new Compute();
+            long answer[] = obj.printFirstNegativeInteger(a, n, k);
+            int sz = answer.length;
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<sz;i++)
+                output.append(answer[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    
+    public long[] printFirstNegativeInteger(long A[], int N, int K)
+    {
+        int i=0; int j=0;
+        long[] ans = new long[N-K+1];
+        int count=0;
+        while(j<A.length){
+            if(j-i+1 < K ) j++;
+            else if(j-i+1 == K) {
+                boolean x = false;
+               loop: for(int m = i; m<=j; m++) {
+                    if(A[m]<0){
+                        ans[count] = A[m];
+                        count++;
+                        x=true;
+                        break loop;
+                    } 
+                }
+                i++; j++;
+                if(!x) ans[count++] = 0;
+            }
+        }
+        return ans;
+    }
+}
