312fc50b6983378fc7a7815e563e2a971ec64b1a
Mohit Chourasia <78469901+Mohitx04-dev@users.noreply.github.com>
2023-07-27 16:08:38 +0530

    Added solution - LeetHub

---
+++ Set kth bit - GFG/set-kth-bit.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+           
+            Solution ob = new Solution();
+            int ans = ob.setKthBit(N,K);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int setKthBit(int N,int k){
+        // code here
+        int el = 1;
+        while(k>0) {
+            k--;
+            el=el<<1;
+        }
+        // System.out.println(el);
+        return N|el;
+    }
+}
